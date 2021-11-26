c8122fd8c608f191364c3036334011ebe302f873
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2021-11-26 19:37:30 +0000

    Advanced Extensions

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask.templating import render_template
+from flask import render_template, request
 from app import app
 from models.player import Player
 from models.game import Game
@@ -8,7 +8,7 @@ def index():
     return "Hello"
 
 @app.route("/<player_1_choice>/<player_2_choice>")
-def compare(player_1_choice, player_2_choice):
+def compare_url(player_1_choice, player_2_choice):
     player_1 = Player("Player 1",player_1_choice)
     player_2 = Player("Player 2",player_2_choice)
     game = Game()
@@ -18,4 +18,17 @@ def compare(player_1_choice, player_2_choice):
 @app.route("/welcome")
 def welcome():
     return render_template("welcome.html", title="Welcome")
-    
+
+@app.route("/play", methods=["POST"])
+def play():
+    player_choice = request.form["player_1_choice"]
+    player_choice = player_choice.lower()
+    print(player_choice)
+    player_1= Player("Player 1",player_choice)
+    game = Game()
+    computer_choice = game.random_computer_choice()
+    print(computer_choice)
+    player_2 = Player("Computer",computer_choice)
+    winner = game.compare_choices(player_1,player_2)
+    return render_template("index.html", title="Game", player_1=player_1, player_2=player_2, winner=winner )
+
--- models/game.py
+++ models/game.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Game:
 
     def compare_choices(self,player_1,player_2):
@@ -7,21 +9,22 @@ class Game:
                 winning_player = player_2
             elif player_2.choice == "scissors":
                 winning_player = player_1
-
         elif player_1.choice == "scissors":
             if player_2.choice == "paper":
                 winning_player = player_1
             elif player_2.choice == "rock":
                 winning_player = player_2
-
         elif player_1.choice == "paper":
             if player_2.choice == "rock":
                 winning_player = player_1
             elif player_2.choice == "scissors":
-                winning_player = player_2
-        
+                winning_player = player_2      
         return winning_player
 
+    def random_computer_choice(self):
+        choice_list=["rock", "paper", "scissors"]
+        rand_int = randint(0,2)
+        return choice_list[rand_int]
 
 
 
